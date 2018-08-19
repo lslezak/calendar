4da4ccd3f100d3e2a09a76d8fc900448327086fd
Ladislav Slezák <lslezak@suse.cz>
2018-08-19 12:11:35 +0200

    Added CalDavZap Web client

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 
+# Web and CalDav Calendar Docker Application
+
 
 ## DB Init
 
 ```sh
-docker-compose up
-docker-compose run -e PSQLOPTS="--host postgres -U postgres " davical /usr/share/davical/dba/create-database.sh davical admin_password
-docker-compose exec postgres createuser davical_app
-docker-compose exec postgres createuser davical_dba
-# TODO
-# php agendavcli migrations:migrate
+./docker_init.sh
 ```
--- docker_init.sh
+++ docker_init.sh
@@ -3,12 +3,14 @@
 # make sure we are always at the root dir
 cd "${BASH_SOURCE%/*}"
 
+set -e -x
+
 # build the Docker images
 docker-compose build
 
 docker-compose start
 docker-compose run -e PSQLOPTS="--host postgres -U postgres " davical /usr/share/davical/dba/create-database.sh davical admin_password
-docker-compose exec postgres createuser davical_app
-docker-compose exec postgres createuser davical_dba
+# docker-compose exec postgres createuser davical_app
+# docker-compose exec postgres createuser davical_dba
 docker-compose down
 
